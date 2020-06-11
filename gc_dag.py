b2f2a0ef0bd396852fa19acce6fb24e0894b09f3
tienhunggeoguard <tien.hung@geoguard.com>
2020-06-11 16:18:02 +0700

    Add notify environment

--- gc_dag.py
+++ gc_dag.py
@@ -7,6 +7,7 @@ from airflow.utils.dates import days_ago
 
 # Local lib
 from gc_slack import Slacker
+from settings import TEST, PRODUCTION
 
 
 class GCDAG(DAG):
@@ -20,8 +21,12 @@ class GCDAG(DAG):
         )
 
     @staticmethod
-    def get_default_args():
-        return {
+    def get_default_args(
+            notify_failue=True,
+            notify_success=True,
+            notify_retry=True
+    ):
+        default_args = {
             'owner': 'reporter',
             'depends_on_past': False,
             'start_date': days_ago(2),
@@ -35,29 +40,45 @@ class GCDAG(DAG):
             # 'priority_weight': 10,
             # 'end_date': datetime(2016, 1, 1),
             # 'wait_for_downstream': False,
+            # 'execution_timeout': timedelta(seconds=300),
             # 'dag': dag,
             # 'sla': timedelta(hours=2),
-            # 'execution_timeout': timedelta(seconds=300),
-            'on_failure_callback': GCDAG.slack_notificaion,
-            'on_success_callback': GCDAG.slack_notificaion,
-            'on_retry_callback': GCDAG.slack_notificaion,
             # 'sla_miss_callback': GCDAG.slack_notificaion,
             # 'trigger_rule': 'all_success'
         }
+        if notify_failue:
+            default_args['on_failure_callback'] = GCDAG.slack_notificaion
+        if notify_success:
+            default_args['on_success_callback'] = GCDAG.slack_notificaion
+        if notify_retry:
+            default_args['on_retry_callback'] = GCDAG.slack_notificaion
 
     def __init__(
             self,
+            environment: Optional[
+                TEST, PRODUCTION
+            ],
             title: str,
             description: str = '',
             schedule_interval: Optional[
                 Union[str, timedelta, relativedelta]
             ] = timedelta(days=1),
             dag_default_args: dict = {},
+            notify_failue=True,
+            notify_success=True,
+            notify_retry=True,
             *args,
             **kwargs
     ):
+
         if not dag_default_args:
-            dag_default_args = GCDAG.get_default_args()
+            dag_default_args = GCDAG.get_default_args(
+                notify_failue,
+                notify_success,
+                notify_retry
+            )
+        # Setup env
+        self.environment = environment
 
         super().__init__(
             title,
---
+++ rp_2099.py
@@ -0,0 +1,6 @@
+"""
+Author: Hung
+Email: tien.hung@geoguard.com
+The last update: 2020-06-11
+Python version: 3.8.0
+"""
