61e302798963d6308f831e25c38a01ab5d327908
tienhunggeoguard <tien.hung@geoguard.com>
2020-06-15 11:38:59 +0700

    RP-2099: Switch to use common DAG

--- rp_2099.py
+++ rp_2099.py
@@ -5,20 +5,20 @@ from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators import BashOperator
 
-
 default_args = {
-        'owner': 'hung',
-        'depends_on_past': False,
-        'start_date': days_ago(2),
-        'email': ['tien.hung@geoguard.com'],
-        'email_on_failure': True,
-        'email_on_retry': True,
-        'retries': 1,
-        'retry_delay': timedelta(minutes=5),
+    'owner': 'hung',
+    'depends_on_past': False,
+    'start_date': days_ago(2),
+    'email': ['tien.hung@geoguard.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
 }
 
 dag = DAG(
-    'RP-2099 New domain check',
+    'RP-2099 ',
+    description='New domain check',
     schedule_interval='00 8 * * *',
     default_args=default_args,
 )
@@ -30,49 +30,82 @@ py38 = Variable.get('py38')
 report_dir = Variable.get('prod_reports_dir')
 script_dir = f"{report_dir}/rp_2099_migrate_new_domain"
 
-
-def get_command(setting_file):
-    """
-    Get run command for RP-2099
-    @param setting_file:
-    @return:
-    """
-    return " && ".join(
-        [
-            f"export PYTHONPATH={gc_report_libs}",
-            f"{py38}",
-            f"{script_dir}/rp_2099.py {setting_file} "
-            f"-e prod --draft n "
-            f"--dry-run n",
-        ]
-    )
-
+templated_command = """
+export PYTHONPATH={{ gc_report_libs }}
+&& {{ py38 }} {{ script_dir }}/rp_2099.py {{ setting_file }} -e prod --draft n 
+--dry-run n 
+"""
 
 rp_2083 = BashOperator(
-    task_id='RP_2083 ELS Gaming',
-    bash_command=get_command(f"{script_dir}/rp_2083.ini"),
+    task_id='rp_2083',
+    bash_command=templated_command,
+    params=dict(
+        gc_report_libs=gc_report_libs,
+        py38=py38,
+        script_dir=script_dir,
+        setting_file=f"{script_dir}/rp_2083.ini"
+    ),
     dag=dag)
+rp_2083.doc_md = """\
+# RP_2083 ELS Gaming
+"""
 
 rp_1388 = BashOperator(
-    task_id='RP_1388 North Carolina Lottery, North Dakota Lottery ('
-            'Scientific Games) ',
-    bash_command=get_command(f"{script_dir}/rp_1388.ini"),
+    task_id='rp_1388',
+    bash_command=templated_command,
+    params=dict(
+        gc_report_libs=gc_report_libs,
+        py38=py38,
+        script_dir=script_dir,
+        setting_file=f"{script_dir}/rp_1388.ini"
+    ),
     dag=dag)
+rp_1388.doc_md = """\
+# RP_1388 North Carolina Lottery, North Dakota Lottery ('
+'Scientific Games) 
+"""
 
 cs_1285 = BashOperator(
-    task_id='CS_1285 NorthCarolina Neogames',
-    bash_command=get_command(f"{script_dir}/cs_1285.ini"),
+    task_id='cs_1285',
+    bash_command=templated_command,
+    params=dict(
+        gc_report_libs=gc_report_libs,
+        py38=py38,
+        script_dir=script_dir,
+        setting_file=f"{script_dir}/cs_1285.ini"
+    ),
     dag=dag)
+rp_1388.doc_md = """\
+# CS_1285 NorthCarolina Neogames
+"""
 
 rp_966 = BashOperator(
-    task_id='RP_966 US4 Production',
-    bash_command=get_command(f"{script_dir}/rp_966.ini"),
+    task_id='rp_966',
+    bash_command=templated_command,
+    params=dict(
+        gc_report_libs=gc_report_libs,
+        py38=py38,
+        script_dir=script_dir,
+        setting_file=f"{script_dir}/rp_966.ini"
+    ),
     dag=dag)
+rp_1388.doc_md = """\
+# RP_966 US4 Production
+"""
 
 rp_1228 = BashOperator(
-    task_id='RP_1228 NeoPollard',
-    bash_command=get_command(f"{script_dir}/rp_1228.ini"),
+    task_id='rp_1228',
+    bash_command=templated_command,
+    params=dict(
+        gc_report_libs=gc_report_libs,
+        py38=py38,
+        script_dir=script_dir,
+        setting_file=f"{script_dir}/rp_1228.ini"
+    ),
     dag=dag)
+rp_1388.doc_md = """\
+# RP_1228 NeoPollard
+"""
 
 # Main flow
 [rp_2083, rp_1388, cs_1285, rp_966, rp_1228]
