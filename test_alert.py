f29d93aee1cb3ea3f438da932579fac4572bdad4
DUC-LUONG <duc@geoguard.com>
2020-06-18 10:34:54 +0700

    Add dag to test email

---
+++ test_alert.py
@@ -0,0 +1,50 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator, EmailOperator,\
+    PythonOperator
+
+
+default_args = {
+    'owner': 'max',
+    'start_date': datetime(2020, 6, 17),
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'email': ['duc@geoguard.com'],
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+
+}
+
+
+def error_function():
+    raise Exception('Something wrong')
+
+
+with DAG('test_alert',
+         default_args=default_args,
+         schedule_interval='@once',
+         catchup=False) as dag:
+
+    wont_email = DummyOperator(
+      task_id='wont_email')
+
+    will_email = DummyOperator(
+      task_id='will_email',
+      email_on_failure=True)
+
+    failing_task = PythonOperator(
+        task_id='failing_task',
+        python_callable=error_function)
+
+    send_mail = EmailOperator(
+        task_id="send_mail",
+        to='max@geoguard.com',
+        subject='Test mail',
+        html_content='<p> You have got mail! <p>')
+
+# Work FLow
+wont_email
+will_email
+failing_task
+send_mail
